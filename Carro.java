3e9acd209023a0bd616434c0e672fa4b1a516ee2
unknown <marzediaz@gmail.com>
2022-03-04 20:04:59 -0500

    Mi primer commit

---
+++ Carro.java
@@ -0,0 +1,8 @@
+public class Carro{
+	int kilometraje = 0;
+	public void avanzar(){
+		kilometraje=kilometraje+1
+    }
+	public int obtenerkilometraje(){
+		return kilometraje;
+}
---
+++ MiTercerEjercicio.java
@@ -0,0 +1,11 @@
+public class MiTercerEjercicio{
+    public static void main(String [] args){
+        Carro kia = new Carro();
+	Carro chevrolet = new Carro();
+
+	kia.avanzar();
+	System.out.println(kia.obtenerkilometraje());
+	System.out.println(chevrolet.obtenerkilometraje());
+
+	 }
+}
